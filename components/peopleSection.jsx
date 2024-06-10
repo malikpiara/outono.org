a22830ed5f2b879171f19968d769c395399cbbf2
Malik Piara <malik@hey.com>
2024-06-10 21:31:50 +0200

    Added Dinarte to website.

--- components/peopleSection.jsx
+++ components/peopleSection.jsx
@@ -5,6 +5,28 @@ import Link from "next/link";
 export default function PeopleSection() {
     return (
     <div className="grid gap-8 lg:gap-16 sm:grid-cols-2 md:grid-cols-3 mt-8">
+         <div className="text-center text-gray-500">
+            <Image className="mx-auto mb-4 w-28 h-28 rounded-full grayscale"
+             width={100} height={100} src={'/dinarte.jpeg'} alt="Dinarte Jesus"/>
+              
+              <h3 className="mb-1 text-xl tracking-tight text-gray-900">
+                  Dinarte Jesus
+              </h3>
+              <p>Co-Founder at Athleads</p>
+              <ul className="flex justify-center mt-4 space-x-4">
+                  <li>
+                      <Link href="https://github.com/jdinartejesus" className="hover:text-[#ed4928]">
+                      <Github className="mr-2 h-6 w-6" />
+                      </Link>
+                  </li>
+                  <li>
+                  <a href="https://www.linkedin.com/in/jdinartejesus/" className=" hover:text-[#ed4928]">
+                      <Linkedin className="mr-2 h-6 w-6" />
+                      </a>
+                  </li>
+                  
+              </ul>
+          </div>
           <div className="text-center text-gray-500">
             <Image className="mx-auto mb-4 w-28 h-28 rounded-full"
              width={100} height={100} src={'/malik.jpeg'} alt="Malik Piara"/>
