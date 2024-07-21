f9d18c3c33e519834eb3d20144e7a7e83a4d45d8
Malik Piara <malik@hey.com>
2024-07-21 17:03:51 +0200

    Removed sign up option from the website.

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -1,14 +1,13 @@
-import { login, signup } from './actions'
+import { login } from './actions'
 
 export default function LoginPage() {
   return (
-    <form>
-      <label htmlFor="email">Email:</label>
+    <form className='m-auto flex flex-col max-w-96'>
+      <label htmlFor="email">Email</label>
       <input id="email" name="email" type="email" required />
-      <label htmlFor="password">Password:</label>
+      <label htmlFor="password">Password</label>
       <input id="password" name="password" type="password" required />
-      <button formAction={login}>Log in</button>
-      <button formAction={signup}>Sign up</button>
+      <button formAction={login} className='bg-black text-white rounded-md p-2'>Log in</button>
     </form>
   )
 }
