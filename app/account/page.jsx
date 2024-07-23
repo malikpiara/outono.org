9c99a3ae4aa07a27757158a6b1d575faea4eb6d8
Malik Piara <malik@hey.com>
2024-07-23 19:36:13 +0200

    Protecting account route.

--- app/account/page.jsx
+++ app/account/page.jsx
@@ -1,8 +1,15 @@
 import AccountForm from './account-form'
 import { createClient } from '@/utils/supabase/server'
+import { redirect } from 'next/navigation'
 
 export default async function Account() {
   const supabase = createClient()
+
+  const { data: { session } } = await supabase.auth.getSession()
+
+  if (!session) {
+    redirect('/login')
+  }
 
   const {
     data: { user },
