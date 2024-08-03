a92d781a81c76128fe128819cb43f8529588383d
Malik Piara <malik@hey.com>
2024-08-03 22:42:36 +0200

    Added 4 basic KPIs to the dashboard.

--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -1,6 +1,7 @@
 import { createClient } from '@/utils/supabase/server';
 import { redirect } from 'next/navigation';
 import { BarChartLabel } from './bar-chart-label';
+import KpiCards from './kpi-cards';
 
 export default async function Dashboard() {
   const supabase = createClient();
@@ -18,8 +19,9 @@ export default async function Dashboard() {
   } = await supabase.auth.getUser();
 
   return (
-    <div className="flex justify-center w-screen min-h-screen items-center">
-      <div className="w-[650px]">
+    <div className="flex flex-col gap-6 justify-center w-screen min-h-screen px-4">
+      <KpiCards />
+      <div className="w-full sm:w-1/2">
         <BarChartLabel />
       </div>
     </div>
